59b841270d2aff57cbaa5173d7f29bcde0681380
valtairnascimento <valtairnjr@gmail.com>
2024-03-17 10:22:27 -0300

    feat: create a crud for alura books

---
+++ controladores/livro.js
@@ -0,0 +1,94 @@
+const {
+  getTodosLivros,
+  getLivroPorId,
+  insereLivro,
+  modificaLivro,
+  deletaLivroPorId,
+} = require('../servicos/livro');
+
+function getLivros(req, res) {
+  try {
+    const livros = getTodosLivros();
+    res.send(livros);
+  } catch (error) {
+    res.status(500);
+    res.send(error.response.message);
+  }
+}
+
+function getLivro(req, res) {
+  try {
+    const id = req.params.id;
+    if (id && Number(id)) {
+      const livro = getLivroPorId(id);
+      res.send(livro);
+    } else {
+      res.status(422);
+      res.send('Id inválido');
+    }
+  } catch (error) {
+    res.status(500);
+    res.send(error.response.message);
+  }
+}
+
+function postLivro(req, res) {
+  try {
+    const livroNovo = req.body;
+
+    if (req.body.name) {
+      insereLivro(livroNovo);
+      res.status(201);
+      res.send('Livro inserido com sucesso');
+    } else {
+      res.status(422);
+      res.send('O campo nome é obrigatório');
+    }
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+}
+
+function patchLivro(req, res) {
+  try {
+    const id = req.params.id;
+
+    if (id && Number(id)) {
+      const body = req.body;
+
+      modificaLivro(body, id);
+      res.send('Item Modificado com sucesso');
+    } else {
+      res.status(422);
+      res.send('Id inválido');
+    }
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+}
+
+function deleteLivro(req, res) {
+  try {
+    const id = req.params.id;
+    if (id && Number(id)) {
+      deletaLivroPorId(id);
+      res.send('Item removido com sucesso');
+    } else {
+      res.status(422);
+      res.send('Id inválido');
+    }
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+}
+
+module.exports = {
+  getLivros,
+  getLivro,
+  postLivro,
+  patchLivro,
+  deleteLivro,
+};
---
+++ rotas/livro.js
@@ -0,0 +1,22 @@
+const { Router } = require('express');
+
+const router = Router();
+const {
+  getLivros,
+  getLivro,
+  postLivro,
+  patchLivro,
+  deleteLivro,
+} = require('../controladores/livro');
+
+router.get('/', getLivros);
+
+router.get('/:id', getLivro);
+
+router.post('/', postLivro);
+
+router.patch('/:id', patchLivro);
+
+router.delete('/:id', deleteLivro);
+
+module.exports = router;
